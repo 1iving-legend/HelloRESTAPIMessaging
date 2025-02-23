2d05a758a6c03790169ac925743af922841c581c
vivek pathak <vivekkpaathak@gmail.com>
2025-02-24 00:11:15 +0530

    Vivek Bhaskr ADD. UC-1 Use GET request method

---
+++ RESTAPIMessagingApp/src/main/java/springboot/RESTAPIMessagingApp/RestapiMessagingAppApplication.java
@@ -0,0 +1,13 @@
+package springboot.RESTAPIMessagingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiMessagingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiMessagingAppApplication.class, args);
+	}
+
+}
