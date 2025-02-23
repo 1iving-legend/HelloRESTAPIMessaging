da85c7b2af82237d533810517fcb8e104bf79d98
vivek pathak <vivekkpaathak@gmail.com>
2025-02-24 00:34:47 +0530

    Vivek Bhaskr ADD. UC-5 Use PUT Request Method and pass first name as Path Variable and last name as Query Parameter

--- RESTAPIMessagingApp/src/main/java/springboot/RESTAPIMessagingApp/controller/Hello.java
+++ RESTAPIMessagingApp/src/main/java/springboot/RESTAPIMessagingApp/controller/Hello.java
@@ -32,4 +32,10 @@ public class Hello {
     public String sayHello(@RequestBody UserDTO user) {
         return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz!";
     }
+
+    //UC-05
+    @PutMapping("/put/{firstName}")
+    public String sayHello5(@PathVariable String firstName, @RequestParam String lastName){
+        return "Hello "+ firstName + " " +lastName +" from BridgeLabz";
+    }
 }
