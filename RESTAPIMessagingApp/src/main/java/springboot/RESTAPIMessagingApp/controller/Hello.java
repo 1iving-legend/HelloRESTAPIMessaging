2dc560114d472cdbb563fdab99d91f874af0d8ff
vivek pathak <vivekkpaathak@gmail.com>
2025-02-24 00:21:00 +0530

    Vivek Bhaskr ADD. UC-2 Use GET Use GET Request Method and pass name as

--- RESTAPIMessagingApp/src/main/java/springboot/RESTAPIMessagingApp/controller/Hello.java
+++ RESTAPIMessagingApp/src/main/java/springboot/RESTAPIMessagingApp/controller/Hello.java
@@ -2,6 +2,7 @@ package springboot.RESTAPIMessagingApp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,11 @@ public class Hello {
     public String display()
     {
         return "Hello from Bridge Labz!!";
+    }
+
+    //UC2
+    @GetMapping("/query")
+    public String sayHello2(@RequestParam String name){
+        return "Hello "+ name + " from BridgeLabz";
     }
 }
